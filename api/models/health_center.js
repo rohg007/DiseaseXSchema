666a13ea072540b3d17225ada47308cadc27d5d9
Khyati Goyal <17uec065@lnmiit.ac.in>
2020-06-27 13:27:55 +0530

    routes of signup,signin added

--- api/models/health_center.js
+++ api/models/health_center.js
@@ -2,34 +2,45 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var healthCenterSchema = new Schema({
-    address:{
-        type: String
-    },
-    email:{
-        type: String
-    },
-    contact:{
-        type: String,
-        required: true
-    },
-    name:{
-        type: String,
-        required: true
-    },
-    latlng : {
-        type: String,
-        required: true
-    },
-    incharge: {
-        type: String,
-        required: true
-    },
-    pincode: {
-        type: String
-    },
-    web: {
-        type: String,
-    }
+  address: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  avatar: {
+    type: String,
+  },
+
+  contact: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  latlng: {
+    type: String,
+    required: true,
+  },
+  incharge: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: String,
+  },
+  web: {
+    type: String,
+  },
 });
 
 var HealthCenters = mongoose.model('HealthCenter', healthCenterSchema);
--- api/models/human_case.js
+++ api/models/human_case.js
@@ -2,127 +2,137 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var vaccineSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    scientificName: {
-        type: String,
-        required: true
-    },
-    duration:{
-        type: Number,
-        required: true
-    },
-    forHuman: {
-        type: Boolean,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  scientificName: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  forHuman: {
+    type: Boolean,
+    required: true,
+  },
 });
 
 var livestockSchema = new Schema({
-    breed:{
-        type: String,
-        required: true
-    },
-    population:{
-        type: Number,
-        required: true
-    }
+  breed: {
+    type: String,
+    required: true,
+  },
+  population: {
+    type: Number,
+    required: true,
+  },
 });
 
 var diseaseSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    scientificName: {
-        type: String,
-        required: true
-    },
-    precautions:{
-        type: String,
-        required: true
-    },
-    symptoms:{
-        type: String,
-        required: true
-    },
-    morbidity:{
-        type: Number,
-        required: true
-    }, 
-    mortality: {
-        type: Number,
-        required: true
-    },
-    total_affected: {
-        type: Number,
-        required: true
-    },
-    total_deaths: {
-        type: Number,
-        required: true
-    },
-    livestock: [livestockSchema],
-    vaccine: [vaccineSchema]
+  name: {
+    type: String,
+    required: true,
+  },
+  scientificName: {
+    type: String,
+    required: true,
+  },
+  precautions: {
+    type: String,
+    required: true,
+  },
+  symptoms: {
+    type: String,
+    required: true,
+  },
+  morbidity: {
+    type: Number,
+    required: true,
+  },
+  mortality: {
+    type: Number,
+    required: true,
+  },
+  total_affected: {
+    type: Number,
+    required: true,
+  },
+  total_deaths: {
+    type: Number,
+    required: true,
+  },
+  livestock: [livestockSchema],
+  vaccine: [vaccineSchema],
 });
 
 var healthCenterSchema = new Schema({
-    address:{
-        type: String
-    },
-    email:{
-        type: String
-    },
-    contact:{
-        type: String,
-        required: true
-    },
-    name:{
-        type: String,
-        required: true
-    },
-    latlng : {
-        type: String,
-        required: true
-    },
-    incharge: {
-        type: String,
-        required: true
-    },
-    pincode: {
-        type: String
-    },
-    web: {
-        type: String,
-    }
+  address: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  avatar: {
+    type: String,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  latlng: {
+    type: String,
+    required: true,
+  },
+  incharge: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: String,
+  },
+  web: {
+    type: String,
+  },
 });
 
 var humanCaseSchema = new Schema({
-    patientName:{
-        type: String,
-        required:true
-    },
-    patientAddress:{
-        type: String
-    },
-    patientEmail:{
-        type: String
-    },
-    patientContact:{
-        type: String,
-        required: true
-    },
-    latlng:{
-        type: String,
-    },
-    disease: {
-        type: diseaseSchema
-    },
-    healthCenter: {
-        type: healthCenterSchema
-    }
+  patientName: {
+    type: String,
+    required: true,
+  },
+  patientAddress: {
+    type: String,
+  },
+  patientEmail: {
+    type: String,
+  },
+  patientContact: {
+    type: String,
+    required: true,
+  },
+  latlng: {
+    type: String,
+  },
+  disease: {
+    type: diseaseSchema,
+  },
+  healthCenter: {
+    type: healthCenterSchema,
+  },
 });
 
 module.exports = mongoose.model('HumanCase', humanCaseSchema);
