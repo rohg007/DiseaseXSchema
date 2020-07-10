06b71ac36d7615a72dc671ec499fdf1d740e37a9
Hitesha10 <sharmahitesha7@gmail.com>
2020-07-10 16:14:44 +0530

    new-files-uploaded

--- client/src/components/Animal_Case.js
+++ client/src/components/Animal_Case.js
@@ -12,7 +12,6 @@ import {
   Input,
 } from 'reactstrap';
 
-import Search from './Search';
 import editImage from '../images/edit.png';
 const filtereddata = data.animalCaseSchema.filter((temp) => {
   return temp.healthCenter.email === 'suraksha@yahoo.com';
