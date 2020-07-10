05fd712bc1e28b899b436abef2727d2b22c1447f
Hitesha10 <sharmahitesha7@gmail.com>
2020-07-10 10:57:33 +0530

    thrash

--- client/src/components/Health_center.js
+++ client/src/components/Health_center.js
@@ -41,7 +41,14 @@ const Health_center = () => {
     <Bar
       data={state}
       options={{
-        title: { display: true, text: `Health Center : ${filtereddata[0].name} status`,fontSize:'20' },
+        title: {
+          display: true,
+
+          position: 'top',
+          text: `HEALTH CENTER : ${filtereddata[0].name} status`,
+          fontSize: '20',
+          fontColor: 'black',
+        },
         legend: {
           display: false,
           position: 'right',
@@ -52,49 +59,86 @@ const Health_center = () => {
 
   return (
     <div style={sectionStyle}>
-      <div style={{ width: '50%', height: '50%', justifyContent: 'center' }}>
-        {barChart}
+      <div className='row'>
+        <div
+          style={{
+            width: '50%',
+            height: '50%',
+            justifyContent: 'center',
+            paddingLeft: '5%',
+          }}
+        >
+          {barChart}
+        </div>
+
+        <div
+          class='btn-group mr-7'
+          role='group'
+          aria-label='First group'
+          style={{ paddingLeft: '8%', paddingTop: '5%', height: '2%' }}
+        >
+          <a class='btn btn-large btn-dark' href='/'>
+            NEW HUMAN CASE
+          </a>
+          <a class='btn btn-large btn-dark' href='/'>
+            NEW ANIMAL CASE
+          </a>
+          <a class='btn btn-large btn-dark' href='/human_case'>
+            HUMAN CASES
+          </a>
+          <a class='btn btn-large btn-dark' href='/animal_case'>
+            ANIMAL CASES
+          </a>
+        </div>
       </div>
-      {subdisease.map((dise, i) => {
-        return (
-          <div style={{ width: '30%', height: '30%', padding: '2%' }}>
-            <Doughnut
-              data={{
-                labels: ['Infected', 'Recoverd', 'Deaths'],
-                datasets: [
-                  {
-                    label: 'Rainfall',
-                    backgroundColor: [
-                      'rgba(0, 0, 255, 0.7)',
-                      'rgba(0, 255, 0, 0.7)',
-                      'rgba(255, 0, 0, 0.7)',
-                    ],
-                    hoverBackgroundColor: ['blue', 'green', 'red'],
-                    data: [
-                      dise.total_affected,
-                      dise.total_recovered,
-                      dise.total_deaths,
-                    ],
+      <div className='row'>
+        {' '}
+        {subdisease.map((dise, i) => {
+          return (
+            <div style={{ width: '25%', height: '25%', paddingTop: '3%' }}>
+              <Doughnut
+                data={{
+                  labels: ['Infected', 'Recoverd', 'Deaths'],
+                  datasets: [
+                    {
+                      label: 'Rainfall',
+                      backgroundColor: [
+                        'rgba(0, 0, 255, 0.7)',
+                        'rgba(0, 255, 0, 0.7)',
+                        'rgba(255, 0, 0, 0.7)',
+                      ],
+                      hoverBackgroundColor: ['blue', 'green', 'red'],
+                      data: [
+                        dise.total_affected,
+                        dise.total_recovered,
+                        dise.total_deaths,
+                      ],
+                    },
+                  ],
+                }}
+                options={{
+                  title: {
+                    display: true,
+                    text: dise.name,
+                    fontColor: 'black',
+                    fontSize: 15,
+                    position: 'top',
                   },
-                ],
-              }}
-              options={{
-                title: {
-                  display: true,
-                  text: dise.name,
-                  fontColor: 'black',
-                  fontSize: 20,
-                  position: 'top',
-                },
-                legend: {
-                  display: true,
-                  position: 'right',
-                },
-              }}
-            />
-          </div>
-        );
-      })}
+                  legend: {
+                    display: true,
+                    fontColor: 'black',
+                    fontSize: '2',
+                    position: 'right',
+                  },
+                  labels: {
+                    fontColor: 'black',
+                  },
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
