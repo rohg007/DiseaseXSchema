16a59f20d9a8dcb445f9954375827389b777b778
Khyati Goyal <17uec065@lnmiit.ac.in>
2020-07-29 00:31:54 +0530

    forgot to change the axios

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://diseasex-backend.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:5000/';
+axios.defaults.baseURL = 'https://diseasex-backend.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:5000/';
 export default axios;
